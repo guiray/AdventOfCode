62a31cb45cade720d276357f040d3b5a786d45f6
GuiRay <guiray@users.noreply.github.com>
2021-12-07 00:03:53 -0500

    day 1 part 1

--- AdventOfCode/DAY_01/program.cs
+++ AdventOfCode/DAY_01/program.cs
@@ -1,11 +1,23 @@
-﻿string[] lines = File.ReadAllLines(@"D:\Documents\Github\AdventOfCode\AdventOfCode\DAY_01\input.txt");
+﻿
+string[] lines = File.ReadAllLines(@"D:\Documents\Github\AdventOfCode\AdventOfCode\DAY_01\input.txt");
+
+int lastValue = 0;
+int currentValue = int.Parse(lines[0]);
+
+int counter = 0;
 
 
-
-foreach (string line in lines)
+for (int i = 1; i < lines.Length; i++)
 {
-    // Use a tab to indent each line of the file.
-    Console.WriteLine("\t" + line);
+
+    if (currentValue > lastValue)
+    {
+        counter++;
+    }
+
+    lastValue = currentValue;
+    currentValue = int.Parse(lines[i]);
+
 }
 
-
+Console.WriteLine("Part 1 = " + counter);
