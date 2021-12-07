8722537d8661297dae67c49441f378e7dca349a2
GuiRay <guiray@users.noreply.github.com>
2021-12-06 23:43:04 -0500

    file reader try

---
+++ AdventOfCode/DAY_01/program.cs
@@ -0,0 +1,11 @@
+﻿string[] lines = File.ReadAllLines(@"D:\Documents\Github\AdventOfCode\AdventOfCode\DAY_01\input.txt");
+
+
+
+foreach (string line in lines)
+{
+    // Use a tab to indent each line of the file.
+    Console.WriteLine("\t" + line);
+}
+
+
