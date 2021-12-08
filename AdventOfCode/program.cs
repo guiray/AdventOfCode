c808f96189d3943a3e5e58653e84fc6f3d0359d1
GuiRay <guiray@users.noreply.github.com>
2021-12-08 18:21:36 -0500

    cleaned project and created a class for day1

---
+++ AdventOfCode/program.cs
@@ -0,0 +1,9 @@
+﻿using AdventOfCode.solvers;
+
+
+
+
+day01.Part1();
+
+day01.Part2();
+
---
+++ AdventOfCode/solvers/day01.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.solvers
+{
+    public class day01
+    {
+
+        static string[] lines = File.ReadAllLines(@"../../../Inputs/input_day01.txt");
+
+        
+
+        public static void Part1()
+        {
+            int lastValue = 0;
+            int currentValue = int.Parse(lines[0]);
+
+            int counter = 0;
+            for (int i = 1; i<lines.Length; i++)
+            {
+
+                if (currentValue > lastValue)
+                {
+                    counter++;
+                }
+
+                lastValue = currentValue;
+                currentValue = int.Parse(lines[i]);
+
+            }
+
+            Console.WriteLine("Part 1 = " + counter);
+        }
+
+        public static void Part2()
+        {
+
+            Console.WriteLine("Part 2 = ");
+        }
+
+
+    }
+}
