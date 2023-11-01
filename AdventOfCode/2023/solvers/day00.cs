ee7ac242b5875f2fc42e1cea7ad51f7b64d7baca
Guillaume Raymond <guiray@users.noreply.github.com>
2023-10-31 23:42:21 -0400

    updated to .NET 7.0, and updated template file

--- AdventOfCode/2023/solvers/day00.cs
+++ AdventOfCode/2023/solvers/day00.cs
@@ -4,30 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AdventOfCode2023.solvers
+namespace AdventOfCode2023.solvers;
+
+public class day00
 {
-    public class day00
+    private string[] linesStr;
+
+    public day00()
     {
-        private string[] linesStr;
-
-        public day00()
-        {
-            linesStr = File.ReadAllLines(@"../../../2023/inputs/input_day00.txt");
-        }
-
-        public void Part1()
-        {
-
-
-            Console.WriteLine("Part 1 = ");
-        }
-
-        public void Part2()
-        {
-
-
-            Console.WriteLine("Part 2 = ");
-        }
-
+        linesStr = File.ReadAllLines(@"../../../2023/inputs/input_day00.txt");
     }
+
+    public void Part1()
+    {
+
+
+        Console.WriteLine("Part 1 = ");
+    }
+
+    public void Part2()
+    {
+
+
+        Console.WriteLine("Part 2 = ");
+    }
+
 }
+
