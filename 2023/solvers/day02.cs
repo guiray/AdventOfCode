f6bdcf7ac325c370227879f9e2fd8b5e07a563f3
Guillaume Raymond <guiray@users.noreply.github.com>
2023-12-02 13:17:01 -0500

    Day02 part 2

--- 2023/solvers/day02.cs
+++ 2023/solvers/day02.cs
@@ -60,9 +60,55 @@ public class Day02 : Base
 
     public void Part2()
     {
+        int gameID;
+        int powerOfSetsSum = 0;
+
+        for (int i = 0; i < linesStr.Length; i++)
+        {
+            gameID = i+1;
+
+            // Split all sets of one game
+            String[] separator = { "Game " + gameID + ": ", "; " }; 
+            String[] gameSets = linesStr[i].Split(separator, StringSplitOptions.RemoveEmptyEntries);
+
+            int minBlue = 0;
+            int minRed = 0;
+            int minGreen = 0;
+
+            // for each set from one game
+            for(int j = 0; j < gameSets.Length; j++)
+            {
+                String[] currentGameCubes = gameSets[j].Split(", ");
+
+                // for each cube color in one set
+                for (int k = 0; k < currentGameCubes.Length; k++)
+                {
+                    string[] currentCubeColor = currentGameCubes[k].Split(" ");
+                    int currentCubeAmount = Int32.Parse(currentCubeColor[0]);
+
+                    if (currentCubeColor[1] == "blue" && currentCubeAmount > minBlue)
+                    {
+                        minBlue = currentCubeAmount;
+                    }
+                    else if (currentCubeColor[1] == "red" && currentCubeAmount > minRed)
+                    {
+                        minRed = currentCubeAmount;
+                    }
+                    else if (currentCubeColor[1] == "green" && currentCubeAmount > minGreen)
+                    {
+                        minGreen = currentCubeAmount;
+                    }
+                    
+                }
+
+            }
 
 
-        Console.WriteLine("Part 2 = ");
+            powerOfSetsSum += minBlue * minGreen * minRed;
+            
+        }
+
+        Console.WriteLine("Part 2 = " + powerOfSetsSum);
     }
 
 }
